9b001eb32c927118f016781fd545541582314f0c
Raul Lora Gomez <lgraul87@gmail.com>
2021-06-05 21:51:06 +0200

    using env and urls

--- src/app/_shared/services/candidates.service.ts
+++ src/app/_shared/services/candidates.service.ts
@@ -2,28 +2,35 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
+
 import { Candidate } from 'src/app/_shared/interfaces/candidate';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class CandidatesService {
+
+    private readonly url = environment.url;
+
     constructor(private http: HttpClient) { }
 
     getCandidates(): Observable<Candidate[]> {
-        return this.http.get<Candidate[]>("http://localhost:3000/candidates");
+        return this.http.get<Candidate[]>(`${this.url}/candidates`);
     }
 
     getCandidate(id: string): Observable<Candidate> {
-        return this.http.get<Candidate>(`http://localhost:3000/candidates/${id}`);
+        return this.http.get<Candidate>(`${this.url}/candidates/${id}`);
     }
 
     addCandidate(candidate: Candidate): Observable<Candidate> {
-        return this.http.post<Candidate>("http://localhost:3000/candidates", candidate);
+        return this.http.post<Candidate>(`${this.url}/candidates`, candidate);
     }
+
     editCandidate(candidate: Candidate): Observable<Candidate> {
-        return this.http.put<Candidate>("http://localhost:3000/candidates/" + candidate.id, candidate);
+        return this.http.put<Candidate>(`${this.url}/candidates/${candidate.id}`, candidate);
     }
+
     deleteCandidate(id: string): Observable<Candidate> {
         return this.http.delete<Candidate>(`http://localhost:3000/candidates/${id}`);
     }
--- src/app/_shared/services/employees.service.ts
+++ src/app/_shared/services/employees.service.ts
@@ -2,17 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
+
 import { Employee } from '../interfaces/employee';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeesService {
 
+  private readonly url = environment.url;
+
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<Employee[]> {
-    return this.http.get<Employee[]>("http://localhost:3000/employees");
+    return this.http.get<Employee[]>(`${this.url}/employees`);
   }
 
   getEmployee(id: string): Observable<Employee> {
@@ -20,11 +24,11 @@ export class EmployeesService {
   }
 
   addEmployee(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>("http://localhost:3000/employees", employee);
+    return this.http.post<Employee>(`${this.url}/employees/`, employee);
   }
 
   editEmployee(employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>("http://localhost:3000/employees/" + employee.id, employee);
+    return this.http.put<Employee>(`${this.url}/employees/${employee.id}`, employee);
   }
 
   deleteEmployee(id:string): Observable<Employee>{
